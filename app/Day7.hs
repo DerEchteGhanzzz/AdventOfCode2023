0966b655e980948ec9c3c49be4bcdb828802a322
Ghanzzz <blankensteijnhans@gmail.com>
2023-12-07 11:31:15 +0100

    niceify Day 7

--- app/Day7.hs
+++ app/Day7.hs
@@ -11,6 +11,9 @@ import Data.List.Utils (replace)
 day7 :: AocDay
 day7 = MkDay 7 solveA solveB
 
+data Card = W | R2 | R3 | R4 | R5 | R6 | R7 | R8 | R9 | T | J | Q | K | A
+  deriving (Show, Eq, Enum, Ord)
+
 toCard :: Char -> Card
 toCard 'A' = A
 toCard 'K' = K
@@ -20,17 +23,14 @@ toCard 'T' = T
 toCard 'W' = W
 toCard i   = if isDigit i then toEnum (read [i] - 1) else error "invalid card"
 
-data Card = W | R2 | R3 | R4 | R5 | R6 | R7 | R8 | R9 | T | J | Q | K | A
-  deriving (Show, Eq, Enum, Ord)
-
 data Hand = MkHand {unhand :: [Card], getBid :: Int}
   deriving (Show, Eq)
 
-instance Ord Hand where
-  hand1 <= hand2 = if toHandtype hand1 == toHandtype hand2 then unhand hand1 < unhand hand2 else toHandtype hand1 < toHandtype hand2
-
 data HandType = HighCard | OnePair | TwoPair | ThreeOfAKind | FullHouse | FourOfAKind | FiveOfAKind
   deriving (Show, Enum, Eq, Ord)
+
+instance Ord Hand where
+  hand1 <= hand2 = (toHandtype hand1, unhand hand1) <= (toHandtype hand2, unhand hand2)
 
 cardFreqs :: Hand -> [(Card, Int)]
 cardFreqs hand = toList $ foldr (\card m -> case card `M.lookup` m of
@@ -46,17 +46,17 @@ toHandtype hand | highestFreq    == 5 = FiveOfAKind
                 | length freqMap == 4 = OnePair
                 | highestFreq    == 1 = HighCard
                 | otherwise = error $ show hand ++ ", " ++ show freqMap ++ " is not a valid hand"
-
   where
-    freqMap = removeJs wAmt . reverse . sortOn snd . cardFreqs $ hand
-    wAmt = length . filter (==W) $ unhand hand
+    freqMap = removeWilds wildcardAmt . sortOn (Data.Ord.Down . snd) . cardFreqs $ hand
+    wildcardAmt = length . filter (==W) $ unhand hand
     highestFreq = (snd . head) freqMap
 
-removeJs :: Int -> [(Card, Int)] -> [(Card, Int)]
-removeJs i [] = []
-removeJs 5 freqs = freqs
-removeJs i ((W,f):freqs) = removeJs i freqs
-removeJs i ((c,f):freqs) = (c, f+i) : removeJs 0 freqs
+removeWilds :: Int -> [(Card, Int)] -> [(Card, Int)]
+removeWilds 0 freqs = freqs
+removeWilds 5 freqs = freqs
+removeWilds i [] = []
+removeWilds i ((W,f):freqs) = removeWilds i freqs
+removeWilds i ((c,f):freqs) = (c, f+i) : filter ((/=W) . fst) freqs
 
 solveA :: [String] -> String
 solveA = show . snd . foldl (\(idx, tot) h -> (idx+1, tot + (idx * getBid h))) (1, 0) . sort . parseInput
@@ -66,6 +66,3 @@ solveB = show . snd . foldl (\(idx, tot) h -> (idx+1, tot + (idx * getBid h))) (
 
 parseInput :: [String] -> [Hand]
 parseInput = map (\line -> let [cards, bid] = words line in MkHand (map toCard cards) (read bid))
-
-sortDesc :: Ord a => [a] -> [a]
-sortDesc = sortBy (comparing Data.Ord.Down)
